3e4534e0168b7997bff39c41906220c8fba9b5a2
Rémi Verchère <rverchere@neotion.com>
2016-11-13 22:20:06 +0100

    add script..

---
+++ rename.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if ImageMagick is installed
+identify --version >/dev/null 2>&1 || { echo "ImageMagick is not installerd, exiting" >&2; exit 1; }
+
+output='output'
+outwidth='2560'
+
+mkdir ${output}
+
+for i in `find . -iname "*.jpg"`; do
+    d=`identify -format %[exif:DateTimeOriginal] ${i} | cut -d' ' -f1 | sed "s/:/-/g"`
+    h=`identify -format %[exif:DateTimeOriginal] ${i} | cut -d' ' -f2 | sed "s/:/-/g"`
+    b=`basename ${i} | cut -d'_' -f2`
+    echo "convert ${i} to ${output}/${d}_${h}_${b}"
+    convert -resize ${outwidth} $i ${output}/${d}_${h}_${b}
+done
